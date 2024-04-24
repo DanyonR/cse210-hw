bd0dad13a35bcde9d38aa5b425bc0bbc2d0e20b0
DanyonR <linux_husk@outlook.com>
2024-04-24 13:27:49 -0700

    Rewrite of CSE110 team grade calculator in C#

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,66 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+
+        char letter = ' ';
+
+        if (percentage >= 90)
+        {
+            letter = 'A';
+        }
+        else if (percentage >= 80)
+        {
+            letter = 'B';
+        }
+        else if (percentage >= 70)
+        {
+            letter = 'C';
+        }
+        else if (percentage >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        if ((percentage <= 96) && (!(percentage < 60)))
+        {
+            int lastDigit = percentage % 10;
+
+            char sign = ' ';
+
+            if (lastDigit < 3)
+            {
+                sign = '-';
+            }
+            else if (lastDigit >= 7)
+            {
+                sign = '+';
+            }
+
+            Console.WriteLine($"Your grade is {letter}{sign}");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letter}");
+        }
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("You have not passed the class.");
+        }
     }
 }
