f8a1f0f37dddfbd2dd7204f00bc069d3c42f5b43
DanyonR <linux_husk@outlook.com>
2024-04-26 19:13:18 -0700

    Wrote a program that demonstrates the basics of C# functions.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(name, square);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string userEntry = Console.ReadLine();
+        int number = int.Parse(userEntry);
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int squared = number * number;
+        return squared;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}.");
     }
 }
