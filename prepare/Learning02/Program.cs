60ed647b6c408a9cfd1c5aafc9ca38420f6dcf07
DanyonR <linux_husk@outlook.com>
2024-04-30 20:33:34 -0700

    Wrote a simple resume program in C# demonstrating application of classes and instances.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "White Star Line";
+        job1._jobTitle = "Bilge Operator";
+        job1._startYear = 1912;
+        job1._endYear = 1912;
+
+        Job job2 = new Job();
+        job2._company = "Marv's Muck Mart";
+        job2._jobTitle = "Muck Demucker";
+        job2._startYear = 2021;
+        job2._endYear = 2024;
+
+        Resume resume = new Resume();
+        resume._name = "Thaddeus T. Thudpucker";
+        resume._job.Add(job1);
+        resume._job.Add(job2);
+
+        resume.Display();
     }
 }
