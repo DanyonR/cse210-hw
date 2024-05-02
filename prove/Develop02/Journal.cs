36e5ab3d6109fc021e0f56573c59602031da1b50
DanyonR <linux_husk@outlook.com>
2024-05-02 16:21:40 -0700

    Stubbed out a C# journaling program

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void SaveToFile(string file)
+    {
+
+    }
+    public void LoadFromFile(string file)
+    {
+
+    }
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+    public void DisplayEntries()
+    {
+
+    }
+}
